66c181c39fe47134b701d81ad5a07e6733897612
sb <sb@trint.com>
2020-06-21 14:58:57 +0100

    wip

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,5 +1,11 @@
 export { add } from './add'
 export { sub } from './sub'
 
-export const multiply = (a: number, b: number) => a * b
-export const divide = (a: number, b: number) => a / b
+export const multiply = (a: number, b: number) => {
+  console.log('a * b', a * b)
+  return a * b
+}
+export const divide = (a: number, b: number) => {
+  // console.log(a / b, 'a / b')
+  return a / b
+}
