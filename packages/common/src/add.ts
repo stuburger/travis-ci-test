b618a234787eda325ba1a1542088ddb43833e56c
sb <sb@trint.com>
2020-06-27 17:40:21 +0100

    blah wip 2

--- packages/common/src/add.ts
+++ packages/common/src/add.ts
@@ -2,5 +2,6 @@ export function add(a: number, b: number) {
   console.log('adding...')
   console.log('a + b = ', a + b)
   console.log('a + b = ', a + b)
+  console.log('a + b = ', a + b)
   return a + b
 }
