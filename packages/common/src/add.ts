5490d85638f4f14374723a1f58410f7f22bd3e60
sb <sb@trint.com>
2020-06-27 17:47:35 +0100

    should not run ci

--- packages/common/src/add.ts
+++ packages/common/src/add.ts
@@ -2,6 +2,5 @@ export function add(a: number, b: number) {
   console.log('adding...')
   console.log('a + b = ', a + b)
   console.log('a + b = ', a + b)
-  console.log('a + b = ', a + b)
   return a + b
 }
