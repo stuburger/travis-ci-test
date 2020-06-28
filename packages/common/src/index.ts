16d17c04a076d9999caf20499a7826760ad17b36
sb <sb@trint.com>
2020-06-28 09:47:21 +0100

    blah

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -4,6 +4,7 @@ export { sub } from './sub'
 export const multiply = (a: number, b: number) => {
   console.log('multiplying...')
   console.log('a * b', a * b)
+  console.log('blah')
   return a * b
 }
 export const divide = (a: number, b: number) => {
